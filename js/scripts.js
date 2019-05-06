52e5c88ed4595cd49ad979cc1186126a23978ef4
Allan Kamau <akamau41@gmail.com>
2019-05-06 09:05:23 +0300

    add jquery scripts

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#design").click(function() {
+    $(".img1").slideToggle();
+    $("#par1").toggle();
+  });
+
+   $("#development").click(function() {
+     $(".img2").slideToggle();
+     $("#par2").toggle();
+   });
+
+   $(".product").click(function() {
+     $("#img3").slideToggle();
+     $("#par3").toggle();
+   });
+
+
+
+});
+
+
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+});
